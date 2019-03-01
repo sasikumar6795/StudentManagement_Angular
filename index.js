fcb8f8c3ece546673decfda5d2e4ae6d1a802e56
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2019-03-01 16:21:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const { mongoose } = require('./db.js');
+
+var app = express();
+app.use(bodyparser.json());
+app.listen(3000, () => console.log('server started at port : 3000'));
